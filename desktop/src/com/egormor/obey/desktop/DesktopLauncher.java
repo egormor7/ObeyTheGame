d65102f0a1677c646520d4294fbcd17e12915d6a
Egor <egormor7@gmail.com>
2021-03-15 23:21:53 +0400

    First steps and hud created

---
+++ desktop/src/com/egormor/obey/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.egormor.obey.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.egormor.obey.OBEY;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OBEY(), config);
+	}
+}
