6e2717a4c1ba7eab188ce8f9f405d5ac8009cb82
Egor <egormor7@gmail.com>
2021-03-23 19:40:07 +0400

    Added test map

--- core/src/com/egormor/obey/Screens/PlayScreen.java
+++ core/src/com/egormor/obey/Screens/PlayScreen.java
@@ -1,10 +1,14 @@
 package com.egormor.obey.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -14,15 +18,26 @@ import com.egormor.obey.Scenes.Hud;
 
 public class PlayScreen implements Screen {
     private OBEY game;
+
     private OrthographicCamera gamecam;
     private Viewport gamePort;
     private Hud hud;
+
+    private TmxMapLoader maploader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     public  PlayScreen(OBEY game){
         this.game = game;
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(OBEY.V_WIDTH, OBEY.V_HEIGHT, gamecam);
         hud = new Hud(game.batch);
+        //Gdx.files.internal("/titledMaps");
+
+        maploader = new TmxMapLoader();
+        map = maploader.load("first_level_test_3.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
     }
 
     @Override
@@ -30,10 +45,34 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt){
+        if (Gdx.input.isKeyPressed(Input.Keys.W))
+            gamecam.position.y += 100 * dt;
+        else if (Gdx.input.isKeyPressed(Input.Keys.A))
+            gamecam.position.x -= 100 * dt;
+        else if (Gdx.input.isKeyPressed(Input.Keys.S))
+            gamecam.position.y -= 100 * dt;
+        else if (Gdx.input.isKeyPressed(Input.Keys.D))
+            gamecam.position.x += 100 * dt;
+
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        gamecam.update();
+        renderer.setView(gamecam);
+
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
